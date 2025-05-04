7fab7108ffcbf123b86db07a61909ed73409995a
MaarceloLuiz <marceloluizgpereira@gmail.com>
2025-05-04 19:01:23 +0100

    refactor: change the distance calculator to haversine formula and enhance bearing.go

--- backend/pkg/geography/geocalc/bearing.go
+++ backend/pkg/geography/geocalc/bearing.go
@@ -20,14 +20,22 @@ func getCompass(guessLat, guessLng, answerLat, answerLng float64) string {
 
 func degreesToCompass(degrees float64) string {
 	ranges := map[string][2]float64{
-		"N":  {337.5, 22.5},
-		"NE": {22.5, 67.5},
-		"E":  {67.5, 112.5},
-		"SE": {112.5, 157.5},
-		"S":  {157.5, 202.5},
-		"SW": {202.5, 247.5},
-		"W":  {247.5, 292.5},
-		"NW": {292.5, 337.5},
+		"N":   {354.375, 5.625}, // Narrow range for North
+		"NNE": {5.625, 28.125},
+		"NE":  {28.125, 61.875},
+		"ENE": {61.875, 84.375},
+		"E":   {84.375, 95.625}, // Narrow range for East
+		"ESE": {95.625, 118.125},
+		"SE":  {118.125, 151.875},
+		"SSE": {151.875, 174.375},
+		"S":   {174.375, 185.625}, // Narrow range for South
+		"SSW": {185.625, 208.125},
+		"SW":  {208.125, 241.875},
+		"WSW": {241.875, 264.375},
+		"W":   {264.375, 275.625}, // Narrow range for West
+		"WNW": {275.625, 298.125},
+		"NW":  {298.125, 331.875},
+		"NNW": {331.875, 354.375},
 	}
 
 	// special case for North since it wraps around 360
@@ -57,8 +65,14 @@ func radiansToDegrees(bearing float64) float64 {
 
 // θ = atan2( sin Δλ ⋅ cos φ2 , cos φ1 ⋅ sin φ2 − sin φ1 ⋅ cos φ2 ⋅ cos Δλ )
 func initialBearing(guessLat, guessLng, answerLat, answerLng float64) float64 {
-	y := math.Sin(answerLng-guessLng) * math.Cos(answerLat)
-	x := math.Cos(guessLat)*math.Sin(answerLat) - math.Sin(guessLat)*math.Cos(answerLat)*math.Cos(answerLng-guessLng)
+	guessLatRad := guessLat * (math.Pi / 180)
+	guessLngRad := guessLng * (math.Pi / 180)
+	answerLatRad := answerLat * (math.Pi / 180)
+	answerLngRad := answerLng * (math.Pi / 180)
+	deltaLng := answerLngRad - guessLngRad
+
+	y := math.Sin(deltaLng) * math.Cos(answerLatRad)
+	x := math.Cos(guessLatRad)*math.Sin(answerLatRad) - math.Sin(guessLatRad)*math.Cos(answerLatRad)*math.Cos(answerLngRad-guessLngRad)
 	bearing := math.Atan2(y, x)
 
 	return bearing
